c6750231a86f15e8857f22672f1c765403012059
santo <2007mesanto@gmail.com>
2026-05-19 13:43:32 +0530

    second commit

--- userpasscap.py
+++ userpasscap.py
@@ -1,159 +1,113 @@
 import sqlite3
 import hashlib
+import datetime
 import random
 import string
-import datetime
-
-#DATABASE CONNECTION 
+# Connect to database
 conn = sqlite3.connect("users.db")
 cur = conn.cursor()
-
-# Create table
 cur.execute("""
 CREATE TABLE IF NOT EXISTS users(
     username TEXT,
     password TEXT
 )
 """)
-
-#USER REGISTRATION 
-username = input("Enter Username: ")
-
-# Password validation loop
-while True:
-
-    password = input("Enter Password: ")
-
-    upper = False
-    lower = False
-    digit = False
-    symbol = False
-    space = False
-
-    for ch in password:
-        if ch.isupper():
-            upper = True
-
-        elif ch.islower():
-            lower = True
-
-        elif ch.isdigit():
-            digit = True
-
-        elif ch.isspace():
-            space = True
-
-        else:
-            symbol = True
-
-    # Password Conditions
-    if len(password) < 8:
-        print("Weak Password - Must contain at least 8 characters")
-
-    elif not password[0].isalpha():
-        print("Weak Password - Must start with an alphabet")
-
-    elif not upper:
-        print("Weak Password - Must contain an uppercase letter")
-
-    elif not lower:
-        print("Weak Password - Must contain a lowercase letter")
-
-    elif not digit:
-        print("Weak Password - Must contain a digit")
-
-    elif not symbol:
-        print("Weak Password - Must contain a symbol")
-
-    elif space:
-        print("Weak Password - Should not contain spaces")
-
+cur.execute("DELETE FROM users")
+username=input("enter the user name ")
+password=input("enter the password")
+upper=False
+lower=False
+space=False
+symbol=False
+digit=False
+ar=0
+for s in password:
+    if s.isupper()==True:
+        upper=True
+    elif s.islower()==True :
+        lower=True
+    elif s.isdigit()==True:
+        digit=True
+    elif s.isspace==True:
+        space=True
     else:
-        print("Strong Password")
-        break
-
-#STORE USERNAME AND PASSWORD
-hashed_password = hashlib.sha256(password.encode()).hexdigest()
-
-cur.execute(
-    "INSERT INTO users VALUES(?, ?)",
-    (username, hashed_password)
-)
-
-conn.commit()
-
-# LOGIN SECTION 
-print("\n LOGIN ")
-
-login_username = input("Re-enter Username: ")
-login_password = input("Re-enter Password: ")
-
-hashed_login_password = hashlib.sha256(
-    login_password.encode()
-).hexdigest()
-
-# Check username and password
-cur.execute(
-    "SELECT * FROM users WHERE username=? AND password=?",
-    (login_username, hashed_login_password)
-)
-
-result = cur.fetchone()
-
-# CAPTCHA SECTION 
-if result:
-
-    captcha = ''.join(
-        random.choices(
-            string.ascii_letters + string.digits,
-            k=6
-        )
-    )
-
-    print("\nCaptcha:", captcha)
-
-    user_captcha = input("Enter Captcha: ")
-
-    if user_captcha == captcha:
-
-        print("\nLogin Successful")
-
-        now = datetime.datetime.now()
-
-        # Write login details into text file
-        with open("login_details.txt", "a") as f:
-
-            f.write("\nLOGIN SUCCESSFUL\n")
-            f.write("Username : " + login_username + "\n")
-            f.write("Date     : " + str(now.date()) + "\n")
-            f.write("Time     : " + str(now.time()) + "\n")
-
+        symbol=True
+if password[0].isalpha()==False:
+    print("password must start with alphabets")
+    password=input("enter correct form  password")
+    exit()
+for  a in password:
+    if a==ar:
+        print("not possible")
+        exit()
     else:
-        print("\nIncorrect Captcha")
+        ar=a
 
-        now = datetime.datetime.now()
-
-        with open("login_details.txt", "a") as f:
-
-            f.write("\n LOGIN FAILED\n")
-            f.write("Reason   : Incorrect Captcha\n")
-            f.write("Username : " + login_username + "\n")
-            f.write("Date     : " + str(now.date()) + "\n")
-            f.write("Time     : " + str(now.time()) + "\n")
-
+if len(password)<8:
+    print(" weak password ,password atleast contain only 8 character ") 
+    password=input("enter correct form  password")
+elif not upper:
+    print("weak password,password contain uppercase letter")
+    password=input("enter correct form  password")
+elif not  lower:
+    print("weak password,password contain atlest one lowercase")
+    password=input("enter correct form  password")
+elif not  digit:
+    print("weak password ,password contain atleast one digit")
+    password=input("enter correct form  password")
+elif   space:
+    print("weak password,password not contain any space")
+    password=input("enter correct form  password")
+elif not  symbol:
+    print("weak password,password not contain any symbol")
+    password=input("enter correct form  password")
 else:
+    print("strong password") 
+hashpas=hashlib.sha256(password.encode())
+hashed=hashpas.hexdigest()
+cur.execute("INSERT INTO users VALUES(?, ?)", (username, hashed))
+conn.commit()
+newusername = input("Enter username: ")
+newpassword = input("Enter password: ")
+hashpasd=hashlib.sha256(newpassword.encode())
+nhashed=hashpasd.hexdigest()
 
-    print("\nInvalid Username or Password")
+# Secure query using parameterized statements
+cur.execute(
+    "SELECT * FROM users WHERE username = ? AND password = ?",
+    (newusername, nhashed)
+)
+result = cur.fetchone()
+captcha = ''.join(random.choices(
+    string.ascii_letters + string.digits, k=6))
+attempts = 3
+while attempts>0:
+    print("\nCaptcha:", captcha)
+    user_input = input("Enter the captcha: ")
+# Check login
+    if result and user_input==captcha:
+        print("login sucess")
+        with open("file1.txt","a") as f:
+            now=str(datetime.datetime.now())
+            f.write("\n")
+            f.write("\n")
+            f.write("login sucess")
+            f.write("\n")
+            f.write("USERNAME:")
+            f.write(newusername)
+            f.write("\n")
+            f.write("DATE AND TIME:")
+            f.write(now)
+            f.write("\n")
 
-    now = datetime.datetime.now()
-
-    with open("login_details.txt", "a") as f:
-
-        f.write("\nLOGIN FAILED \n")
-        f.write("Reason   : Invalid Username or Password\n")
-        f.write("Username : " + login_username + "\n")
-        f.write("Date     : " + str(now.date()) + "\n")
-        f.write("Time     : " + str(now.time()) + "\n")
-
-# ---------------- CLOSE DATABASE ----------------
+        break    
+    
+    else:
+        attempts -= 1
+        print(" Wrong Captcha")
+        if attempts > 0:
+            print("Remaining Attempts:", attempts)
+        else:
+            print(" Login Failed")
+# Close connection
 conn.close()
